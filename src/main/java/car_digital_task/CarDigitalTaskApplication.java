6211b2e439ca0d3b022442e96f17a65cc99d9b04
Gyokhan <gyokhan28@gmail.com>
2025-05-28 17:27:02 +0300

    Add swagger, change home endpoint / to lead to swagger ui

--- src/main/java/car_digital_task/CarDigitalTaskApplication.java
+++ src/main/java/car_digital_task/CarDigitalTaskApplication.java
@@ -1,8 +1,15 @@
 package car_digital_task;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Car Digital Project",
+				description = "Backend API"
+		))
 @SpringBootApplication
 public class CarDigitalTaskApplication {
 
